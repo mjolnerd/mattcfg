02df8cc6dbdbfa3df234fd320218247154b9bdc0
Matt Shultz <mshultz@myrtle.megan.mjolnerd.com>
2017-07-22 18:46:43 -0700

    Script to install on new machine.
    Stolen shamefully from https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/

---
+++ mattcfg_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Your current bashrc needs:
+#alias mattcfg='/usr/bin/git --git-dir=$HOME/.mattcfg/ --work-tree=$HOME'
+# 
+
+git clone --bare https://github.com/mjolnerd/mattcfg.git $HOME/.mattcfg
+function mattcfg {
+   /usr/bin/git --git-dir=$HOME/.mattcfg/ --work-tree=$HOME $@
+}
+mkdir -pv .config-backup
+mattcfg checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    mattcfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+mattcfg checkout
+mattcfg config status.showUntrackedFiles no
